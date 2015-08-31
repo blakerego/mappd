89e050dd9188396fefc8f6f6accac881a0442669
blake <blake.rego@gmail.com>
2015-08-30 20:07:40 -0400

    Creating authentication table to keep track of different oauth providers for users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def home
+    if current_user
+      @user = current_user
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   get '/auth', to: 'sessions#create'
-  get '/auth/foursquare/callback', to: 'sessions#foursquare'
+  get '/auth/foursquare/callback', to: 'sessions#create'
 
 
   # Example of regular route:
