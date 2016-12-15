606f7b47d3f4d8ee1fe3ecc5590bd749dec0210c
blake <blake.rego@gmail.com>
2016-12-15 18:16:12 -0500

    fixing app name.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+
   resources :maps
+
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'home#index'
-  get '/auth', to: 'sessions#create'
-  get '/auth/:provider/callback', to: 'sessions#create'
 
+  get '/auth', to: 'sessions#create'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
 
   get '/users/current', to: 'users#currentUser'
 
